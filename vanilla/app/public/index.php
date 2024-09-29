e4b4a9c4a2860458a50fc54c677addfad41f3e13
tdopierala <tomasz@dopierala.net.pl>
2024-09-29 19:58:31 +0200

    init

---
+++ vanilla/app/public/index.php
@@ -0,0 +1,16 @@
+<?php
+phpinfo();
+
+/*
+$pdo = new PDO('mysql:dbname=omicron;host=mysql', 'dopiet', 'tomek', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+// $query = $pdo->query('SHOW VARIABLES like "version"');
+
+$query = $pdo->query('SHOW DATABASES;');
+
+$row = $query->fetch();
+
+// echo 'MySQL version:' . $row['Value'];
+
+var_dump($row);
+*/
